c8fbd9f793fdbf96e31be3c1371accaf56a1a794
enlian <frejaenlian@gmail.com>
2024-09-30 22:59:35 +0900

    fix bug

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
+const { connectToDatabase, closeConnection } = require('./config/database');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -12,6 +13,24 @@ var loginRouter = require('./routes/login'); // 引入新的 auth 路由
 var app = express();
 // 使用 CORS 中间件
 app.use(cors());
+
+//连接数据库
+app.get('/ping', async (req, res) => {
+  try {
+    const client = await connectToDatabase(); // 连接或获取现有连接
+    await client.db("admin").command({ ping: 1 });
+    res.status(200).send("Successfully connected to MongoDB!");
+  } catch (error) {
+    res.status(500).send("Error connecting to MongoDB.");
+  }
+});
+
+// 在应用关闭时关闭数据库连接
+process.on('SIGINT', async () => {
+  await closeConnection();
+  console.log("MongoDB connection closed.");
+  process.exit(0);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
