3c709fd0d881d147c2fc1c7c30d2722ea5db722a
enlian <frejaenlian@gmail.com>
2024-10-06 02:12:15 +0900

    fix bug

--- frontend/src/client/common/header.tsx
+++ frontend/src/client/common/header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
 import { CategoriesContext } from "../contexts/CategoriesContext";
